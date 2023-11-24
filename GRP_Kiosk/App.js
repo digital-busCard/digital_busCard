12c11a3b95207d304af6d9c8e33757205115d201
chayakornk <chayakorn.kaewlaor@exxonmobil.com>
2023-11-23 20:43:34 -0600

    finish with kiosk

--- GRP_Kiosk/App.js
+++ GRP_Kiosk/App.js
@@ -8,39 +8,12 @@
 
 import React from 'react';
 import Entry from './app/Entry';
-import Realm from 'realm';
 import {createRealmContext} from '@realm/react';
-import { Passenger } from './app/components/Passenger';
-import axios from 'axios';
 import * as encoding from 'fast-text-encoding'
-
-const realmConfig = {
-  schema: [Passenger],
-};
-export const realm = new Realm({schema: [Passenger]});
-
-const {RealmProvider, useRealm, useObject, useQuery} =
-  createRealmContext(realmConfig);
-
-  const addStandard = () => {
-    console.log("adding Standard")
-    realm.write(() => {
-      realm.deleteAll('Passenger')
-      realm.create('Passenger', {
-        uuid: '7eb096d2-8268-11ee-b962-0242ac120000',
-        expireDate: new Date(),
-        _id: new Realm.BSON.ObjectId(),
-      });
-      console.log(realm.objects("Passenger"))
-    });
-  };  
-  addStandard();
 
 const App: () => React$Node = () => {
   return (
-    <RealmProvider>
       <Entry></Entry>
-    </RealmProvider>
   );
 };
 
--- GRP_Kiosk/app/Entry.js
+++ GRP_Kiosk/app/Entry.js
@@ -15,17 +15,14 @@ import {NativeEventEmitter, NativeModules} from 'react-native';
 
 import update from 'immutability-helper';
 import BLEAdvertiser from 'react-native-ble-advertiser';
-import UUIDGenerator from 'react-native-uuid-generator';
 import {PermissionsAndroid} from 'react-native';
-import { createPassenger, getPassenger } from './components/Passenger';
-import { submitAlert } from './components/Alert';
 import { Client } from '@stomp/stompjs';
+import { Colors } from './constant/Colors';
 
 
 // Uses the Apple code to pick up iPhones
 const APPLE_ID = 0x4c;
 const MANUF_DATA = [1, 0];
-// No scanner filters (finds all devices inc iPhone). Use UUID suffix to filter scans if using.
 const SCAN_MANUF_DATA = Platform.OS === 'android' ? null : MANUF_DATA;
 
 
@@ -37,8 +34,8 @@ export async function requestLocationPermission() {
       const granted = await PermissionsAndroid.request(
         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
         {
-          title: 'BLE Avertiser Example App',
-          message: 'Example App access to your location ',
+          title: 'Regina Transit Kiosk',
+          message: 'In order for the app to work properly, please turn on bluetooth',
         },
       );
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
@@ -60,7 +57,7 @@ export async function requestLocationPermission() {
 
     if (!blueoothActive) {
       await Alert.alert(
-        'Example requires bluetooth to be enabled',
+        'The App requires bluetooth to be enabled',
         'Would you like to enable Bluetooth?',
         [
           {
@@ -86,187 +83,78 @@ class Entry extends Component {
     this.state = {
       uuid: '7eb096d2-8268-11ee-b962-0242ac120000',
       isLogging: false,
-      devicesFound: [],
-      passengerCount: 0
+      passengerLogged: []
     };
   }
 
- 
-
+onBTSStatusChange = () => {
+  return
+}
 
 connect = () => {
   console.log("connecting.")
-  if (!this.stompClient.connected()) {
+  if (!this.stompClient || !this.stompClient.connected) {
     console.log("connecting.")
     this.stompClient = new Client({
-      brokerURL: 'https://grp-bus-server-dev.onrender.com/stream?token=kiosk1',
-      onConnect: () => this.onConnected(),
+      brokerURL: 'https://grp-bus-server-dev.onrender.com/stream?token=bus1',
+      onConnect: () => this.onConnected()
     })
     this.stompClient.activate();
-    console.log("check stat " + this.stompClient.connected())
+    console.log(this.stompClient.connected)
   }
 }
 
 onConnected = () => {
   console.log("onConnected");
-  console.log("check stat " + this.stompClient.status)
-  // Subscribe to the Public Topic
-  this.stompClient.subscribe("/topic/new-passenger", (messeage) => {
-    console.log("message " + messeage)
-    createPassenger(messeage)
+  console.log("ws stat: " + this.stompClient.connected)
+  this.stompClient.subscribe("/user/topic/passengers", (messeage) => {
+    console.log("in subscribe")
+    console.log("message received: " + messeage.body)
+    this.addPassengerLogged(JSON.parse(messeage.body))
   });
+  console.log("after stat: " + this.stompClient.connected)
 }
 
-authenticate(event) {
-  console.log("in authenticate " + event.uuid)
-  console.log(this.state.isConnected)
-  if (this.state.isConnected) {
-    console.log("is connect")
-    // if (!this.isTimedOut(event)) {
-    //   console.log("timeout" + event.uuid)
-    //   return
-    // }
-    console.log("not timedout")
-    authenticatedEvent = this.validate(event)
-    authenticatedEvent.verification = this.state.passengerCount + 1
-    this.setState({
-      passengerCount: {$set: (authenticatedEvent.verification)}
-    })
-    this.setState({
-      passengerCount: {$set: (authenticatedEvent.verification)}
-    })
-    this.timeOut(authenticatedEvent, authenticatedEvent.verification)
-    this.alert(authenticatedEvent)
-  }
-}
-
-
-isTimedOut(event) {
-  if (event.expire != null && event.expire > new Date()) {
-    return true
-  }
-}
-
-validate(event) {
-  console.log("validate..")
-  passengers = getPassenger();
-  passengers.forEach((passenger) => {
-    console.log(passenger)
-    console.log(passenger.uuid.toUpperCase() + "=" + event.uuid.toUpperCase())
-    if (passenger.uuid.toUpperCase() === event.uuid.toUpperCase()) {
-      console.log("return " + passenger.uuid)
-      this.validatedPassenger = passenger;
-    }
-  })
-  return this.validatedPassenger;
-}
-
-alert(event) {
-  const toSubmit = {
-    passengers: [{
-      passengerId: event.uuid,
-      verification: event.verification
-    }],
-    bus: "10"
-  }
-  console.log("constructing payload " + toSubmit.passengerId.toString())
-  submitAlert(this.stompClient, toSubmit)
-}
-
-timeOut(event, verification) {
-  console.log("timeout")
-  console.log(event)
-  const expire = new Date();
-  console.log(expire)
-  expire.setMinutes(expire.getMinutes() + 30);
-  const index = this.state.devicesFound.findIndex(({uuid}) => uuid.toUpperCase() === event.uuid.toUpperCase());
-  console.log(this.state.devicesFound)
-  console.log("changing value " + index)
-  console.log(expire)
-  this.setState({
-    devicesFound: update(this.state.devicesFound, {
-      [index]: {
-        expire: {$set: expire},
-        verification: {$set: verification}
-      },
-    }),
-  });
-  console.log(this.state.devicesFound[index]);
-}
-
-  addDevice(_uuid, _name, _mac, _rssi, _date) {
-    const index = this.state.devicesFound.findIndex(({uuid}) => uuid === _uuid);
-    if (index < 0) {
-      let newDev = {
-        uuid: _uuid,
-        name: _name,
-        mac: _mac,
-        rssi: _rssi,
-        start: _date,
-        end: _date,
-        expire: null,
+  addPassengerLogged(passengerInformation) {
+    const queuedPassenger = this.state.passengerLogged.find((passenger) => passenger.verification == passengerInformation.verification)
+    console.log(queuedPassenger)
+    if (!queuedPassenger) {
+      console.log("New Queue is validated")
+      let newPassenger = {
+        uuid: passengerInformation.passengerId,
+        verification: passengerInformation.verification,
+        expireDate: passengerInformation.expireDate
       };
 
       this.setState({
-        devicesFound: update(this.state.devicesFound, {
+        passengerLogged: update(this.state.passengerLogged, {
           $push: [
-            newDev
+            newPassenger
           ],
-        }),
-      });
-      console.log("authenticate");
-      this.authenticate(newDev);
-    } else {
-      this.setState({
-        devicesFound: update(this.state.devicesFound, {
-          [index]: {
-            end: {$set: _date},
-            rssi: {$set: _rssi || this.state.devicesFound[index].rssi},
-          },
-        }),
-      });
+        })
+      })
     }
   }
 
   componentDidMount() {
     requestLocationPermission();
-    UUIDGenerator.getRandomUUID((newUid) => {
-      this.setState({
-        uuid: '7eb096d2-8268-11ee-b962-0242ac120000',
-        isConnected: false
-      });
+    this.setState({
+      uuid: '7eb096d2-8268-11ee-b962-0242ac120000',
+      isConnected: false
     });
     this.connect();
-  }
-
-  componentWillUnmount() {
-    if (this.state.isLogging) {
-      this.stop();
-    }
+    this.start();
+    setInterval(() => {
+      this.setState({
+        time : new Date().toLocaleString()
+      })
+    }, 1000);
   }
 
   start() {
     console.log("stomp connection.. " + this.stompClient)
     this.connect();
     console.log(this.state.uuid, 'Registering Listener');
-    const eventEmitter = new NativeEventEmitter(NativeModules.BLEAdvertiser);
-
-    this.onDeviceFound = eventEmitter.addListener('onDeviceFound', (event) => {
-      if (event.serviceUuids) {
-        for (let i = 0; i < event.serviceUuids.length; i++) {
-          if (event.serviceUuids[i] && event.rssi < 40) {
-            this.addDevice(
-              event.serviceUuids[i],
-              event.deviceName,
-              event.deviceAddress,
-              event.rssi,
-              new Date(),
-            );
-          }
-        }
-      }
-    });
-
     console.log(this.state.uuid, 'Starting Advertising');
     BLEAdvertiser.broadcast('7eb096d2-8268-11ee-b962-0242ac120000', MANUF_DATA, {
       advertiseMode: BLEAdvertiser.ADVERTISE_MODE_BALANCED,
@@ -278,43 +166,14 @@ timeOut(event, verification) {
       .then((sucess) => console.log('7eb096d2-8268-11ee-b962-0242ac120000', 'Adv Successful', sucess))
       .catch((error) => console.log('7eb096d2-8268-11ee-b962-0242ac120000', 'Adv Error', error));
 
-    console.log(this.state.uuid, 'Starting Scanner');
-    BLEAdvertiser.scan(SCAN_MANUF_DATA, {
-      scanMode: BLEAdvertiser.SCAN_MODE_LOW_LATENCY,
-    })
-      .then((sucess) => console.log(this.state.uuid, 'Scan Successful', sucess))
-      .catch((error) => console.log(this.state.uuid, 'Scan Error', error));
-
     this.setState({
       isLogging: true,
     });
   }
 
-  stop() {
-    console.log(this.state.uuid, 'Removing Listener');
-    this.onDeviceFound.remove();
-    delete this.onDeviceFound;
-
-    console.log(this.state.uuid, 'Stopping Broadcast');
-    BLEAdvertiser.stopBroadcast()
-      .then((sucess) => console.log(this.state.uuid, 'Stop Broadcast Successful', sucess))
-      .catch((error) => console.log(this.state.uuid, 'Stop Broadcast Error', error));
-
-    console.log(this.state.uuid, 'Stopping Scanning');
-    BLEAdvertiser.stopScan()
-      .then((sucess) => console.log(this.state.uuid, 'Stop Scan Successful', sucess))
-      .catch((error) => console.log(this.state.uuid, 'Stop Scan Error', error));
-
-    this.setState({
-      isLogging: false,
-    });
-    this.connect();
-  }
-
   short(str) {
     return (
-      str.substring(0, 4) +
-      ' ... ' +
+      ' xxx' +
       str.substring(str.length - 4, str.length)
     ).toUpperCase();
   }
@@ -324,66 +183,33 @@ timeOut(event, verification) {
       <SafeAreaView>
         <View style={styles.body}>
           <View style={styles.sectionContainer}>
-            <Text style={styles.sectionTitle}>GRP BUS Receveiver</Text>
-            <Text style={styles.sectionDescription}>
-              Broadcasting:{' '}
-              <Text style={styles.highlight}>
-                {this.short(this.state.uuid)}
-              </Text>
-            </Text>
+            <Text style={styles.sectionTitle}>BUS 1 Kiosk</Text>
+            <Text style={styles.sectionTitle}>Welcome!</Text>
           </View>
-
-          <View style={styles.sectionContainer}>
-            {this.state.isLogging ? (
-              <TouchableOpacity
-                onPress={() => this.stop()}
-                style={styles.stopLoggingButtonTouchable}>
-                <Text style={styles.stopLoggingButtonText}>Stop</Text>
-              </TouchableOpacity>
-            ) : (
-              <TouchableOpacity
-                onPress={() => this.start()}
-                style={styles.startLoggingButtonTouchable}>
-                <Text style={styles.startLoggingButtonText}>Start</Text>
-              </TouchableOpacity>
-            )}
-          </View>
-
+          <Text style={styles.bold}>{this.state.time}</Text>
           <View style={styles.sectionContainerFlex}>
-            <Text style={styles.sectionTitle}>Devices Around</Text>
+            <Text style={styles.sectionTitle}>Authenticated Passenger</Text>
             <FlatList
-              data={this.state.devicesFound}
+              data={this.state.passengerLogged.sort((a, b) => { 
+                return (parseInt(b.verification) - parseInt(a.verification))
+              })}
               renderItem={({item}) => (
-                <Text style={styles.itemPastConnections}>
-                  {(item.uuid)} {item.mac} {item.rssi}
-                </Text>
+                <View style={styles.passenger}>
+                <View style={styles.containerVerification}>
+                  <Text style={styles.textVerification}>N{item.verification}</Text>
+                </View>
+                <View style={styles.details}>
+                  <View style={styles.containerId}>
+                    <Text style={styles.textId}>{this.short(item.uuid)}</Text>
+                  </View>
+                  <View style={styles.containerExpireDate}>
+                    <Text style={styles.textExpireDate}>expire: {item.expireDate}</Text>
+                  </View>
+                </View>
+              </View>
               )}
-              keyExtractor={(item) => item.uuid}
+              keyExtractor={(item) => item.verification}
             />
-          </View>
-
-          <View style={styles.sectionContainerFlex}>
-            <Text style={styles.sectionTitle}>Devices Authenticated Around</Text>
-            <FlatList
-              data={this.state.devicesFound}
-              inverted={true}
-              renderItem={({item}) => (
-                item.expire != null ?
-                <Text style={styles.itemPastConnections}>
-                  {(item.uuid)} {item.verification} {item.rssi} 
-                </Text>
-                : null
-              )}
-              keyExtractor={(item) => item.uuid}
-            />
-          </View>
-
-          <View style={styles.sectionContainer}>
-            <TouchableOpacity
-              onPress={() => this.setState({devicesFound: []})}
-              style={styles.startLoggingButtonTouchable}>
-              <Text style={styles.startLoggingButtonText}>Clear Devices</Text>
-            </TouchableOpacity>
           </View>
         </View>
       </SafeAreaView>
@@ -413,53 +239,15 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     textAlign: 'center',
   },
-  sectionDescription: {
-    fontSize: 18,
-    fontWeight: '400',
-    textAlign: 'center',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  startLoggingButtonTouchable: {
-    borderRadius: 12,
-    backgroundColor: '#665eff',
-    height: 52,
-    alignSelf: 'center',
-    width: 300,
-    justifyContent: 'center',
-  },
-  startLoggingButtonText: {
-    fontSize: 14,
-    lineHeight: 19,
-    letterSpacing: 0,
-    textAlign: 'center',
-    color: '#ffffff',
-  },
-  stopLoggingButtonTouchable: {
-    borderRadius: 12,
-    backgroundColor: '#fd4a4a',
-    height: 52,
-    alignSelf: 'center',
-    width: 300,
-    justifyContent: 'center',
-  },
-  stopLoggingButtonText: {
-    fontSize: 14,
-    lineHeight: 19,
-    letterSpacing: 0,
-    textAlign: 'center',
-    color: '#ffffff',
-  },
-  listPastConnections: {
-    width: '80%',
-    height: 200,
-  },
-  itemPastConnections: {
-    padding: 3,
-    fontSize: 18,
-    fontWeight: '400',
-  },
+  bold: {textAlign: 'center', fontSize: 20, fontWeight: 'bold'},
+  passenger: {fontSize: 30, height: 100, borderRadius: 30, flexDirection: 'row', alignContent: 'center', alignItems: 'center', justifyContent: 'center', marginBottom: 20},
+  containerVerification: {flex: 1, height: '100%', justifyContent: 'center', alignContent: 'center', textAlign: 'center', backgroundColor: Colors.primary, borderTopLeftRadius: 30, borderBottomLeftRadius: 30},
+  textVerification: {textAlign: 'center', fontSize: 20, fontWeight: 'bold', color: 'white'},
+  details: {flexDirection: 'column', paddingVertical: 20, flex: 2, height: '100%', justifyContent: 'center', borderColor: Colors.primary, borderTopWidth: 2, borderBottomWidth: 2, borderRightWidth: 2, borderTopRightRadius: 30, borderBottomRightRadius: 30},
+  containerId: {flex: 1, height: '100%', marginRight: 10, justifyContent: 'center'},
+  textId: {textAlign: 'center', fontSize: 20},
+  containerExpireDate: {flex: 1, height: '100%', justifyContent: 'center', alignContent: 'center', textAlign: 'center'},
+  textExpireDate: {textAlign: 'center', fontSize: 15}
 });
 
 export default Entry;
